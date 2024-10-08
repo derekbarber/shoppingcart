b7179d3d74590883865ea95e968bbe18c1d437c1
Derek Barber <derekbarber@gmail.com>
2024-10-07 21:31:30 -0700

    Update email that is sent upon form submission

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import logo from "$lib/images/pb-logo.jpg";
 
   import { Resend } from 'resend';
+  import { categories, cards } from './cards.js'
 
   const resend = new Resend('re_caxSRCyD_53aJRtXz68CKmATZdtcXwehk');
 
@@ -13,257 +14,7 @@
   let pickup_location = ""
 
   let errors = []
-
-  let categories = [
-    {
-      id: 1,
-      name: "Groceries",
-    },
-    {
-      id: 2,
-      name: "Restaurants",
-    },
-    {
-      id: 3,
-      name: "Clothing / Dept. Stores",
-    },
-    {
-      id: 4,
-      name: "Home Improvement",
-    },
-    {
-      id: 5,
-      name: "Gas Stations",
-    },
-    {
-      id: 6,
-      name: "Specialty Stores",
-    },
-  ];
-
-  let shopping_cards = [
-    {
-      id: 1,
-      category: 1,
-      selected: false,
-      name: "Choices",
-      cards: ["$20", "$100"],
-    },
-    { id: 2, category: 1, selected: false, name: "M&M Meats", cards: ["$25"] },
-    {
-      id: 3,
-      category: 1,
-      selected: false,
-      name: "Safeway / Thriftys",
-      cards: ["$25", "$50", "$100", "$200"],
-    },
-    {
-      id: 4,
-      category: 1,
-      selected: false,
-      name: "Save-On Foods",
-      cards: ["$25", "$50", "$100"],
-    },
-    {
-      id: 5,
-      category: 1,
-      selected: false,
-      name: "Superstore / Extra Foods",
-      cards: ["$25", "$50", "$100", "$250"],
-    },
-    {
-      id: 6,
-      category: 1,
-      selected: false,
-      name: "Trifty Foods",
-      cards: ["$25", "$50", "$100"],
-    },
-    { id: 7, category: 1, selected: false, name: "T&T Market", cards: ["$25"] },
-    /* Restaurants */
-    {
-      id: 20,
-      category: 2,
-      selected: false,
-      name: "Boathouse",
-      cards: ["$25", "$50"],
-    },
-    {
-      id: 21,
-      category: 2,
-      selected: false,
-      name: "Boston Pizza",
-      cards: ["$25", "$50"],
-    },
-    {
-      id: 22,
-      category: 2,
-      selected: false,
-      name: "Browns Social",
-      cards: ["$25", "$50"],
-    },
-    {
-      id: 23,
-      category: 2,
-      selected: false,
-      name: "Cactus Club",
-      cards: ["$25", "$50"],
-    },
-    {
-      id: 24,
-      category: 2,
-      selected: false,
-      name: "Earl's",
-      cards: ["$25", "$50"],
-    },
-    {
-      id: 25,
-      category: 2,
-      selected: false,
-      name: "Keg",
-      cards: ["$25", "$50", "$100"],
-    },
-
-    /*** CLOTHING / DEPT. STORES */
-    {
-      id: 40,
-      category: 3,
-      selected: false,
-      name: "American Eagle",
-      cards: ["$25"],
-    },
-    {
-      id: 41,
-      category: 3,
-      selected: false,
-      name: "Hudson Bay / Home Outfitters",
-      cards: ["$25", "$50", "$100"],
-    },
-    {
-      id: 42,
-      category: 3,
-      selected: false,
-      name: "La Senza",
-      cards: ["$25"],
-    },
-    {
-      id: 43,
-      category: 3,
-      selected: false,
-      name: "Men's Work Warehouse",
-      cards: ["$25", "$50"],
-    },
-    {
-      id: 45,
-      category: 3,
-      selected: false,
-      name: "Options - Old Navy / Banana Rep / Gap",
-      cards: ["$25", "$50"],
-    },
-
-    /*** HOME IMPROVEMENT */
-    {
-      id: 50,
-      category: 4,
-      selected: false,
-      name: "Canadian Tire",
-      cards: ["$25", "$100"],
-    },
-    {
-      id: 51,
-      category: 4,
-      selected: false,
-      name: "Home Depot",
-      cards: ["$25", "$100"],
-    },
-    {
-      id: 52,
-      category: 4,
-      selected: false,
-      name: "Rona",
-      cards: ["$25", "$50"],
-    },
-
-    /***** GAS STATIONS */
-    {
-      id: 60,
-      category: 5,
-      selected: false,
-      name: "Chevron",
-      cards: ["$25", "$50", "$100"],
-    },
-    {
-      id: 61,
-      category: 5,
-      selected: false,
-      name: "Esso",
-      cards: ["$25", "$100"],
-    },
-    {
-      id: 62,
-      category: 5,
-      selected: false,
-      name: "Petro Canada",
-      cards: ["$25", "$50", "$100"],
-    },
-    {
-      id: 63,
-      category: 5,
-      selected: false,
-      name: "Shell",
-      cards: ["$25", "$50", "$100"],
-    },
-
-    /***** SPECIALTY STORES */
-    {
-      id: 70,
-      category: 6,
-      selected: false,
-      name: "Avis Car Rental",
-      cards: ["$50", "$100"],
-    },
-    {
-      id: 71,
-      category: 6,
-      selected: false,
-      name: "Bath and Body Works",
-      cards: ["$25"],
-    },
-    {
-      id: 72,
-      category: 6,
-      selected: false,
-      name: "Best Buy",
-      cards: ["$25", "$50", "$100", "$250"],
-    },
-    {
-      id: 73,
-      category: 6,
-      selected: false,
-      name: "Chapters",
-      cards: ["$25", "$50"],
-    },
-    {
-      id: 74,
-      category: 6,
-      selected: false,
-      name: "Cineplex",
-      cards: ["$10", "$25"],
-    },
-    {
-      id: 75,
-      category: 6,
-      selected: false,
-      name: "Amazon.ca (not Amazon.com)",
-      cards: ["$25", "$50", "$100"],
-    },
-    {
-      id: 76,
-      category: 6,
-      selected: false,
-      name: "Claires / Icings",
-      cards: ["$25"],
-    },
-  ];
+  let shopping_cards = cards
 
   let purchased_cards = [];
 
@@ -333,7 +84,7 @@
     if (name.length < 3) {
       errors.push("Please enter a name of at least 3 characters")
     }
-    if (email.length == 0 || validateEmail(email)) {
+    if (email.length == 0 || !validateEmail(email)) {
       errors.push("Please enter a valid email")
     }
     if (phone_number.length <  10) {
@@ -342,7 +93,7 @@
     if (pickup_location.length == 0) {
       errors.push("Please select your pickup time and location")
     }
-    if (shopping_cards.length == 0) {
+    if (purchased_cards.length == 0) {
       errors.push("Please select at least one shopping card")
     }
   }
@@ -355,11 +106,25 @@
       return false
     }
 
-    let order_html = `<p>The following order was placed for Shopping Cards</p>\
+    let order_html = `<p>The following order was placed for Shopping Cards.</p><br>\
+      <p>Details of Purchaser</p>
       <p>Name: ${name}</p>\
       <p>Email: ${email}</p>\
-      <p>Phone Number: ${phone_number}</p>\
-    `
+      <p>Phone Number: ${phone_number}</p><br>\
+      <p>How Are they Paying: ${how_paying}</p><br>\
+      <p>Pickup Location: ${pickup_location}</p><br>`
+
+    if (how_paying == 'etransfer') {
+      order_html += `<p>Do they need the passphrase? ${need_passphrase}</p>`
+    }
+
+    order_html += `<br><p>Shopping Cards</p><br>`
+
+    for (const purchased_card of purchased_cards) {
+      order_html += `<p> - {purchased_card.name}: {purchased_card.value} x {purchased_card.quantity}</p>`
+    }
+
+    order_html += `<br><p>TOTAL COST: ${totalCost()}</p>`
 
     resend.emails.send({
       from: 'onboarding@resend.dev',
@@ -494,7 +259,7 @@
 
         <div class="col-span-full">
           <label
-            for="country"
+            for="how_paying"
             class="block text-sm font-medium leading-6 text-gray-900"
             >How will be paying for the gift cards?</label
           >
@@ -552,7 +317,6 @@
           </fieldset>
           </div>
         {/if}
-
 
         <div class="col-span-full">
           <label
---
+++ src/routes/cards.js
@@ -0,0 +1,250 @@
+export let categories = [
+  {
+    id: 1,
+    name: "Groceries",
+  },
+  {
+    id: 2,
+    name: "Restaurants",
+  },
+  {
+    id: 3,
+    name: "Clothing / Dept. Stores",
+  },
+  {
+    id: 4,
+    name: "Home Improvement",
+  },
+  {
+    id: 5,
+    name: "Gas Stations",
+  },
+  {
+    id: 6,
+    name: "Specialty Stores",
+  },
+];
+
+export let cards = [
+  {
+    id: 1,
+    category: 1,
+    selected: false,
+    name: "Choices",
+    cards: ["$20", "$100"],
+  },
+  { id: 2, category: 1, selected: false, name: "M&M Meats", cards: ["$25"] },
+  {
+    id: 3,
+    category: 1,
+    selected: false,
+    name: "Safeway / Thriftys",
+    cards: ["$25", "$50", "$100", "$200"],
+  },
+  {
+    id: 4,
+    category: 1,
+    selected: false,
+    name: "Save-On Foods",
+    cards: ["$25", "$50", "$100"],
+  },
+  {
+    id: 5,
+    category: 1,
+    selected: false,
+    name: "Superstore / Extra Foods",
+    cards: ["$25", "$50", "$100", "$250"],
+  },
+  {
+    id: 6,
+    category: 1,
+    selected: false,
+    name: "Trifty Foods",
+    cards: ["$25", "$50", "$100"],
+  },
+  { id: 7, category: 1, selected: false, name: "T&T Market", cards: ["$25"] },
+  /* Restaurants */
+  {
+    id: 20,
+    category: 2,
+    selected: false,
+    name: "Boathouse",
+    cards: ["$25", "$50"],
+  },
+  {
+    id: 21,
+    category: 2,
+    selected: false,
+    name: "Boston Pizza",
+    cards: ["$25", "$50"],
+  },
+  {
+    id: 22,
+    category: 2,
+    selected: false,
+    name: "Browns Social",
+    cards: ["$25", "$50"],
+  },
+  {
+    id: 23,
+    category: 2,
+    selected: false,
+    name: "Cactus Club",
+    cards: ["$25", "$50"],
+  },
+  {
+    id: 24,
+    category: 2,
+    selected: false,
+    name: "Earl's",
+    cards: ["$25", "$50"],
+  },
+  {
+    id: 25,
+    category: 2,
+    selected: false,
+    name: "Keg",
+    cards: ["$25", "$50", "$100"],
+  },
+
+  /*** CLOTHING / DEPT. STORES */
+  {
+    id: 40,
+    category: 3,
+    selected: false,
+    name: "American Eagle",
+    cards: ["$25"],
+  },
+  {
+    id: 41,
+    category: 3,
+    selected: false,
+    name: "Hudson Bay / Home Outfitters",
+    cards: ["$25", "$50", "$100"],
+  },
+  {
+    id: 42,
+    category: 3,
+    selected: false,
+    name: "La Senza",
+    cards: ["$25"],
+  },
+  {
+    id: 43,
+    category: 3,
+    selected: false,
+    name: "Men's Work Warehouse",
+    cards: ["$25", "$50"],
+  },
+  {
+    id: 45,
+    category: 3,
+    selected: false,
+    name: "Options - Old Navy / Banana Rep / Gap",
+    cards: ["$25", "$50"],
+  },
+
+  /*** HOME IMPROVEMENT */
+  {
+    id: 50,
+    category: 4,
+    selected: false,
+    name: "Canadian Tire",
+    cards: ["$25", "$100"],
+  },
+  {
+    id: 51,
+    category: 4,
+    selected: false,
+    name: "Home Depot",
+    cards: ["$25", "$100"],
+  },
+  {
+    id: 52,
+    category: 4,
+    selected: false,
+    name: "Rona",
+    cards: ["$25", "$50"],
+  },
+
+  /***** GAS STATIONS */
+  {
+    id: 60,
+    category: 5,
+    selected: false,
+    name: "Chevron",
+    cards: ["$25", "$50", "$100"],
+  },
+  {
+    id: 61,
+    category: 5,
+    selected: false,
+    name: "Esso",
+    cards: ["$25", "$100"],
+  },
+  {
+    id: 62,
+    category: 5,
+    selected: false,
+    name: "Petro Canada",
+    cards: ["$25", "$50", "$100"],
+  },
+  {
+    id: 63,
+    category: 5,
+    selected: false,
+    name: "Shell",
+    cards: ["$25", "$50", "$100"],
+  },
+
+  /***** SPECIALTY STORES */
+  {
+    id: 70,
+    category: 6,
+    selected: false,
+    name: "Avis Car Rental",
+    cards: ["$50", "$100"],
+  },
+  {
+    id: 71,
+    category: 6,
+    selected: false,
+    name: "Bath and Body Works",
+    cards: ["$25"],
+  },
+  {
+    id: 72,
+    category: 6,
+    selected: false,
+    name: "Best Buy",
+    cards: ["$25", "$50", "$100", "$250"],
+  },
+  {
+    id: 73,
+    category: 6,
+    selected: false,
+    name: "Chapters",
+    cards: ["$25", "$50"],
+  },
+  {
+    id: 74,
+    category: 6,
+    selected: false,
+    name: "Cineplex",
+    cards: ["$10", "$25"],
+  },
+  {
+    id: 75,
+    category: 6,
+    selected: false,
+    name: "Amazon.ca (not Amazon.com)",
+    cards: ["$25", "$50", "$100"],
+  },
+  {
+    id: 76,
+    category: 6,
+    selected: false,
+    name: "Claires / Icings",
+    cards: ["$25"],
+  },
+];
